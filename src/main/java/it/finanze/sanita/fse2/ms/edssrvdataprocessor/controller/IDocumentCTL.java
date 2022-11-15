0f3a93cff434b856d06aa4f75b061b80b30b5572
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2022-11-15 16:36:43 +0100

    fix(Code): Refactoring hard-coded paths

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdataprocessor/controller/IDocumentCTL.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdataprocessor/controller/IDocumentCTL.java
@@ -3,19 +3,6 @@
  */
 package it.finanze.sanita.fse2.ms.edssrvdataprocessor.controller;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
-
-import it.finanze.sanita.fse2.ms.edssrvdataprocessor.dto.response.error.base.ErrorResponseDTO;
-import it.finanze.sanita.fse2.ms.edssrvdataprocessor.exceptions.DocumentNotFoundException;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -24,14 +11,26 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import it.finanze.sanita.fse2.ms.edssrvdataprocessor.dto.DocumentReferenceDTO;
 import it.finanze.sanita.fse2.ms.edssrvdataprocessor.dto.response.DocumentResponseDTO;
+import it.finanze.sanita.fse2.ms.edssrvdataprocessor.dto.response.error.base.ErrorResponseDTO;
+import it.finanze.sanita.fse2.ms.edssrvdataprocessor.exceptions.DocumentNotFoundException;
 import it.finanze.sanita.fse2.ms.edssrvdataprocessor.exceptions.OperationException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+import static it.finanze.sanita.fse2.ms.edssrvdataprocessor.utility.RoutesUtility.API_PROCESSOR_TAG;
+import static it.finanze.sanita.fse2.ms.edssrvdataprocessor.utility.RoutesUtility.API_PROCESS_PATH;
 
 /**
  * Ingestion Controller.
  * 
  */
-@RequestMapping(path = "/v1")
-@Tag(name = "Document Data Processor Controller")
+@Tag(name = API_PROCESSOR_TAG)
 @Validated
 public interface IDocumentCTL {
 
@@ -45,14 +44,11 @@ public interface IDocumentCTL {
 	 * @throws OperationException  A generic MongoDB Exception 
 	 * @throws DocumentNotFoundException  An exception which is thrown when a document is not found on the FHIR Server 
 	 */
-    @PostMapping(value = "/process", produces = {
-			MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
+    @PostMapping(value = API_PROCESS_PATH, produces = { MediaType.APPLICATION_JSON_VALUE }, consumes = { MediaType.APPLICATION_JSON_VALUE })
     @Operation(summary = "Receives a document to be saved in the staging MongoDB (Update and Delete Flows)", description = "Servizio che consente di salvare un documento alla base dati di staging (Flussi Update e Delete).")
     @ApiResponse(content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = DocumentResponseDTO.class)))
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Creazione Documento avvenuta con successo", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = DocumentResponseDTO.class))),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))) })
     ResponseEntity<DocumentResponseDTO> processOperation(HttpServletRequest request, @RequestBody DocumentReferenceDTO document) throws IOException, OperationException, DocumentNotFoundException;
-
-
 }
---
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdataprocessor/utility/RoutesUtility.java
@@ -0,0 +1,13 @@
+package it.finanze.sanita.fse2.ms.edssrvdataprocessor.utility;
+
+public final class RoutesUtility {
+
+    public static final String API_VERSION = "v1";
+
+    public static final String API_PROCESS = "process";
+
+    public static final String API_PROCESS_PATH = "/" + API_VERSION + "/" + API_PROCESS;
+
+    public static final String API_PROCESSOR_TAG = "Data Processor";
+
+}
