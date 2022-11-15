f4a578894c6153b5cfea069c805f0f2d1ab34fab
Giuseppe Baittiner <Giuseppe.Baittiner-CIC@ibm.com>
2022-11-15 12:44:29 +0100

    fix(DocumentRepo): Adapt to new naming collection system

--- src/main/java/it/finanze/sanita/fse2/ms/edssrvdataprocessor/repository/mongo/impl/DocumentRepo.java
+++ src/main/java/it/finanze/sanita/fse2/ms/edssrvdataprocessor/repository/mongo/impl/DocumentRepo.java
@@ -8,7 +8,6 @@ import it.finanze.sanita.fse2.ms.edssrvdataprocessor.config.Constants;
 import it.finanze.sanita.fse2.ms.edssrvdataprocessor.exceptions.OperationException;
 import it.finanze.sanita.fse2.ms.edssrvdataprocessor.repository.IDocumentRepo;
 import it.finanze.sanita.fse2.ms.edssrvdataprocessor.repository.entity.DocumentReferenceETY;
-import it.finanze.sanita.fse2.ms.edssrvdataprocessor.utility.ProfileUtility;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -19,8 +18,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 /**
- * The document repository implementation 
- *
+ * The document repository implementation
  */
 @Repository
 public class DocumentRepo implements IDocumentRepo {
@@ -31,16 +29,10 @@ public class DocumentRepo implements IDocumentRepo {
 	@Autowired
 	private MongoTemplate mongoTemplate;
 	
-	/**
-	 * Profile Utility 
-	 */
-	@Autowired
-	private ProfileUtility profileUtility;
-	
 	@Override
 	public DocumentReferenceETY insert(DocumentReferenceETY ety) throws OperationException {
 		try {
-			return mongoTemplate.insert(ety, getCollectionName()); 
+			return mongoTemplate.insert(ety);
 		} catch(MongoException ex) {
 			throw new OperationException(Constants.Logs.ERROR_MONGO_INSERT, ex); 
 		}
@@ -49,21 +41,17 @@ public class DocumentRepo implements IDocumentRepo {
 	@Override
 	public void deleteByIdentifier(String identifier) {
 		Query query = Query.query(Criteria.where(Constants.App.IDENTIFIER).is(identifier)); 
-		mongoTemplate.remove(query, getCollectionName()); 	
+		mongoTemplate.remove(query, DocumentReferenceETY.class);
 	}
 	
 	@Override
 	public DocumentReferenceETY findById(String id) {
-		DocumentReferenceETY ety = mongoTemplate.findById(id, DocumentReferenceETY.class, getCollectionName()); 
+		DocumentReferenceETY ety = mongoTemplate.findById(id, DocumentReferenceETY.class);
 		return ObjectUtils.isEmpty(ety) ? null : ety;
 	} 
 	
 	@Override
 	public List<DocumentReferenceETY> findAll() {
-		return mongoTemplate.findAll(DocumentReferenceETY.class, getCollectionName()); 
-	}
-
-	public String getCollectionName() {
-		return profileUtility.isTestProfile() ?  Constants.Profile.TEST_PREFIX + '-' + Constants.ComponentScan.INGESTION_STAGING : Constants.ComponentScan.INGESTION_STAGING;
+		return mongoTemplate.findAll(DocumentReferenceETY.class);
 	}
 }
